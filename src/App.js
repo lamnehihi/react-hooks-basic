3be7e8ba479d4a3b3141de258ed316b6cd2833e1
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-07-24 21:45:26 +0700

    4 - todoList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './app.scss';
-import ColorBox from './components/ColorBox';
-
+import React, { useState } from "react";
+import "./app.scss";
+import ColorBox from "./components/ColorBox";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "I love Easy Frontend! 😍" },
+    { id: 2, title: "We love Easy Frontend! 🥰" },
+    { id: 3, title: "They love Easy Frontend! 🚀" },
+  ]);
+
+  function onTodoClick(item) {
+    const NewTodoList = [...todoList];
+    const index = NewTodoList.indexOf(item);
+    console.log(index);
+    NewTodoList.splice(index, 1)
+    setTodoList(NewTodoList);
+  };
+
   return (
     <div className="app">
       <ColorBox />
+      <TodoList todos={todoList} onTodoClick={onTodoClick} />
     </div>
   );
 }
