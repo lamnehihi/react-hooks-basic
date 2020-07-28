03094c1e75ddc50c85ed55a619f22323ca13fce2
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-07-28 20:40:59 +0700

    7 - PostList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./app.scss";
 import ColorBox from "./components/ColorBox";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
+import PostList from "./components/PostList";
 
 function App() {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const apiUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+        const response = await fetch(apiUrl);
+        const responeJSON = await response.json();
+        
+        const { data } = responeJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log("failed to fetch data : " + error.message);
+      }
+    }
+    fetchPostList();
+  },[])
+
+
   const [todoList, setTodoList] = useState([
     { id: 1, title: "I love Easy Frontend! 😍" },
     { id: 2, title: "We love Easy Frontend! 🥰" },
@@ -35,6 +55,7 @@ function App() {
       <ColorBox />
       <TodoList todos={todoList} onTodoClick={onTodoClick} />
       <TodoForm onSubmit={onTodoFormSubmit}/>
+      <PostList posts={postList} />
     </div>
   );
 }
